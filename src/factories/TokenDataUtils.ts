327769361bb5bd4ddf6d9d56d2e281a54d8ffd7a
Lance Whatley <whatl3y@gmail.com>
2022-03-07 15:23:27 -0500

    update API endpoints

--- src/factories/TokenDataUtils.ts
+++ src/factories/TokenDataUtils.ts
@@ -11,7 +11,7 @@ export default {
   async getTokenPriceUSD(network: string, symbol: string, token: string) {
     const {
       data: { price },
-    } = await axios.get(`https://api.moontography.com/token/price`, {
+    } = await axios.get(`https://api.oklg.com/token/price`, {
       params: {
         network,
         symbol,
