67626911867b310ce8d0017bb153e89ce0a1d50a
Lance Whatley <whatl3y@gmail.com>
2022-04-10 07:37:23 -0400

    Update warnings

--- src/views/layout/AlphaLayout.vue
+++ src/views/layout/AlphaLayout.vue
@@ -2,7 +2,7 @@
 div
   loading-panel(v-if="isLoadingLocal")
   div.d-flex.alert.alert-danger(v-else-if="!isConnected")
-    div.mx-auto(v-if="!isConnected") Please make sure you are connected to your wallet to proceed.
+    div.mx-auto Please make sure you are connected to your wallet to proceed.
   card(v-else)
     template(v-if="!isAlphaValidated")
       div.mb-4
@@ -11,7 +11,7 @@ div
           | the official OKLG website in order to gain access to this functionality.
       div
         div.alert.alert-danger.mb-2(v-if='triedValidatingAndCouldNot')
-          | We could not validate that you have access to alpha as a service.
+          | We could not validate that you have 30,000,000 OKLG and therefore access to alpha as a service.
         div(v-else)
           | Please sign the message below which we use to validate your access to use alpha as a service!
       div
@@ -19,6 +19,10 @@ div
           type="primary"
           @click="signMsg") Sign and Validate AaaS Access
     template(v-else)
+      div.alert.alert-warning
+        | Disclaimer: The data presented with this tool to help you make better informed decisions. We do not
+        | however assume any responsibility for loss due to information provided with this tool.
+        | The information provided is for general informational purposes only. Always DYOR.
       div.mb-2
         i Data refreshes every 10 seconds.
       div.table-full-width.table-responsive.pb-0
@@ -53,13 +57,22 @@ div
                   :href="unescapeLink(row.item.deployerLink)"
                   target="_blank"
                   rel="noopener noreferrer") {{ row.item.deployer }}
-              td {{ row.item.isVerified ? 'Yes' : '' }}
+              td.text-success {{ row.item.isVerified ? 'Yes' : '' }}
               td
                 n-button(
                   type="primary"
                   size="sm"
-                  @click="honeypotCheck(row.item.type === 'New LP Added' ? row.item.token0Info.address : row.item.tokenAddress)")
+                  @click="honeypotCheck(row.item.id, row.item.type === 'New LP Added' ? row.item.token0Info.address : row.item.tokenAddress)")
                     | Check Now
+                //- a(
+                //-   href="#"
+                //-   @click="honeypotCheck(row.item.id, row.item.type === 'New LP Added' ? row.item.token0Info.address : row.item.tokenAddress)")
+                //-   | Check Now
+                div.text-danger.mt-2(v-if="isHoneypot[row.item.id]")
+                  | You currently cannot buy then sell this token. Check it has adequate
+                  | liquidity and trading is enabled, then try again.
+                div.text-success.mt-2(v-else-if="isNotHoneypot[row.item.id]")
+                  | You can buy and sell this token at as little as 25% slippage!
               td {{ parseTimestamp(row.item.timestamp) }}
 </template>
 
@@ -76,6 +89,14 @@ export default {
       isAndHasTriedValidating: null,
       refreshInterval: null,
       refreshIntervalTime: 10000,
+
+      isHoneypot: {
+        // [id]: true
+      },
+
+      isNotHoneypot: {
+        // [id]: true
+      },
 
       tableColumns: [
         { value: "index", text: "#", classes: "" },
@@ -142,17 +163,42 @@ export default {
       this.refreshInterval = null;
     },
 
-    async honeypotCheck(contract) {
+    async honeypotCheck(id, contract) {
+      this.isHoneypot = {
+        ...this.isHoneypot,
+        [id]: false,
+      };
+      this.isNotHoneypot = {
+        ...this.isNotHoneypot,
+        [id]: false,
+      };
+
       const canBuyAndSell = await AlphaUtils.honeypotCheck(
         this.activeNetwork.short_name,
         contract
       );
       if (canBuyAndSell) {
-        this.$toast.success(`You can buy and sell this token!`);
+        // this.$toast.success(`You can buy and sell this token!`);
+        this.isHoneypot = {
+          ...this.isHoneypot,
+          [id]: false,
+        };
+        this.isNotHoneypot = {
+          ...this.isNotHoneypot,
+          [id]: true,
+        };
       } else {
-        this.$toast.error(
-          `It appears you currently cannot buy and subsequently sell this token. Check it has adequate liquidity and trading is turned on, and try again.`
-        );
+        // this.$toast.error(
+        //   `It appears you currently cannot buy and subsequently sell this token. Check it has adequate liquidity and trading is turned on, and try again.`
+        // );
+        this.isHoneypot = {
+          ...this.isHoneypot,
+          [id]: true,
+        };
+        this.isNotHoneypot = {
+          ...this.isNotHoneypot,
+          [id]: false,
+        };
       }
     },
 
